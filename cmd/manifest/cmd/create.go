8d9f585b85bea2f4f1122cbde5241e92afb478ad
Al Cutter <al@google.com>
2023-09-29 13:27:56 +0100

    Migrate to cobra with a create command (#21)

---
+++ cmd/manifest/cmd/create.go
@@ -0,0 +1,117 @@
+// Copyright 2023 The Armored Witness authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package cmd
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/coreos/go-semver/semver"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/transparency-dev/armored-witness-common/release/firmware/ftlog"
+	"golang.org/x/exp/maps"
+)
+
+// createCmd represents the create command
+var (
+	createCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create a new manifest file",
+		Long:  `This command is used to create a new firmware manifest file.\n\nThe manifest describes important details about the firmware. `,
+		Run:   create,
+	}
+
+	// knownFirmwareTypes is the set of possible values for the firmware_type flag.
+	knownFirmwareTypes = map[string]struct{}{
+		ftlog.ComponentApplet:   {},
+		ftlog.ComponentBoot:     {},
+		ftlog.ComponentOS:       {},
+		ftlog.ComponentRecovery: {},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().String("git_tag", "", "The semantic version of the Trusted Applet release.")
+	createCmd.Flags().String("git_commit_fingerprint", "", "Hex-encoded SHA-1 commit hash of the git repository when checked out at the specified git_tag.")
+	createCmd.Flags().String("firmware_file", "", "Path of the firmware ELF file. ")
+	createCmd.Flags().String("tamago_version", "", "The version of the Tamago (https://github.com/usbarmory/tamago) used to compile the Trusted Applet.")
+	createCmd.Flags().String("output_file", "", "The file to write the manifest to. If this is not set, then only print the manifest to stdout.")
+	createCmd.Flags().String("firmware_type", "", fmt.Sprintf("One of %v ", maps.Keys(knownFirmwareTypes)))
+}
+
+func create(cmd *cobra.Command, args []string) {
+	gitTag := requireFlagString(cmd.Flags(), "git_tag")
+	gitCommitFingerprint := requireFlagString(cmd.Flags(), "git_commit_fingerprint")
+	firmwareFile := requireFlagString(cmd.Flags(), "firmware_file")
+	tamagoVersion := requireFlagString(cmd.Flags(), "tamago_version")
+	firmwareType := requireFlagString(cmd.Flags(), "firmware_type")
+	if _, ok := knownFirmwareTypes[firmwareType]; !ok {
+		log.Fatalf("firmware_type must be one of %v", maps.Keys(knownFirmwareTypes))
+	}
+
+	firmwareBytes, err := os.ReadFile(firmwareFile)
+	if err != nil {
+		log.Fatalf("Failed to read firmware_file %q: %v", firmwareFile, err)
+	}
+	digestBytes := sha256.Sum256(firmwareBytes)
+
+	gitTagName, err := semver.NewVersion(gitTag)
+	if err != nil {
+		log.Fatalf("Failed to parse git_tag: %v", err)
+	}
+	tamagoVersionName, err := semver.NewVersion(tamagoVersion)
+	if err != nil {
+		log.Fatalf("Failed to parse tamago_version: %v", err)
+	}
+	r := ftlog.FirmwareRelease{
+		Component:            firmwareType,
+		GitTagName:           *gitTagName,
+		GitCommitFingerprint: gitCommitFingerprint,
+		FirmwareDigestSha256: digestBytes[:],
+		TamagoVersion:        *tamagoVersionName,
+	}
+	b, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+
+	outputFile, _ := cmd.Flags().GetString("output_file")
+	if outputFile == "" {
+		return
+	}
+	if err := os.WriteFile(outputFile, b, 0664); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func requireFlagString(f *pflag.FlagSet, name string) string {
+	v, err := f.GetString(name)
+	if err != nil {
+		log.Fatalf("Getting flag %v: %v", name, err)
+	}
+	if v == "" {
+		log.Fatalf("Flag %v must be speficied", name)
+	}
+	return v
+}
