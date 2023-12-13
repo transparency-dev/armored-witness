da4b75c705d31d80f7a69e9579ae21cc998f5525
Al Cutter <al@google.com>
2023-12-13 13:09:37 +0000

    Remove unneeded parts from api_transparency_dev

--- deployment/api_transparency_dev/main.tf
+++ deployment/api_transparency_dev/main.tf
@@ -1,9 +1,9 @@
 # Configure remote terraform backend for state.
 terraform {
- backend "gcs" {
-   bucket  = "armored-witness-bucket-tfstate"
-   prefix  = "terraform/state"
- }
+  backend "gcs" {
+    bucket = "armored-witness-bucket-tfstate"
+    prefix = "terraform/api.transparency.dev/state"
+  }
 }
 
 # Project
@@ -16,32 +16,11 @@ data "google_project" "project" {
 }
 
 # Enable necessary APIs
-resource "google_project_service" "artifactregistry_googleapis_com" {
-  service = "artifactregistry.googleapis.com"
-}
-resource "google_project_service" "cloudasset_googleapis_com" {
-  service = "cloudasset.googleapis.com"
-}
-resource "google_project_service" "cloudbuild_googleapis_com" {
-  service = "cloudbuild.googleapis.com"
-}
-resource "google_project_service" "cloudfunctions_googleapis_com" {
-  service = "cloudfunctions.googleapis.com"
-}
 resource "google_project_service" "cloudkms_googleapis_com" {
   service = "cloudkms.googleapis.com"
 }
 resource "google_project_service" "compute_googleapis_com" {
   service = "compute.googleapis.com"
-}
-resource "google_project_service" "containerregistry_googleapis_com" {
-  service = "containerregistry.googleapis.com"
-}
-resource "google_project_service" "logging_googleapis_com" {
-  service = "logging.googleapis.com"
-}
-resource "google_project_service" "pubsub_googleapis_com" {
-  service = "pubsub.googleapis.com"
 }
 resource "google_project_service" "serviceusage_googleapis_com" {
   service = "serviceusage.googleapis.com"
@@ -57,7 +36,7 @@ resource "google_project_service" "storage_googleapis_com" {
 }
 
 module "lb-http" {
-  source = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
+  source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
   version = "~> 9.0"
 
   name                  = var.lb_name
@@ -67,9 +46,9 @@ module "lb-http" {
   ssl                             = var.tls
   managed_ssl_certificate_domains = [var.serve_domain]
   https_redirect                  = var.tls
-  
-  create_ipv6_address             = true
-  enable_ipv6                     = true
+
+  create_ipv6_address = true
+  enable_ipv6         = true
 
   backends = {
     default = {
@@ -83,7 +62,7 @@ module "lb-http" {
         }
       ]
 
-    health_check = null
+      health_check = null
 
       enable_cdn = false
 
@@ -170,44 +149,18 @@ resource "google_compute_url_map" "default" {
   }
 }
 
-# GCS buckets
-resource "google_storage_bucket" "armored_witness_firmware" {
-  location                    = "EU"
-  name                        = "armored-witness-firmware"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-resource "google_storage_bucket" "armored_witness_firmware_ci" {
-  location                    = "EU"
-  name                        = "armored-witness-firmware-ci"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-resource "google_storage_bucket" "armored_witness_firmware_log" {
-  location                    = "US"
-  name                        = "armored-witness-firmware-log"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-resource "google_storage_bucket" "armored_witness_firmware_log_ci" {
-  location                    = "US"
-  name                        = "armored-witness-firmware-log-ci"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-}
-
 # Corresponding load balancer backend buckets.
 resource "google_compute_backend_bucket" "firmware_log_ci" {
   name        = "firmware-log-ci-backend"
   description = "Contains CI firmware transparency log"
-  bucket_name = google_storage_bucket.armored_witness_firmware_log_ci.name
+  bucket_name = "armored-witness-firmware-log-ci" # google_storage_bucket.armored_witness_firmware_log_ci.name
   enable_cdn  = false
 }
 
 resource "google_compute_backend_bucket" "firmware_artefacts_ci" {
   name        = "firmware-artefacts-ci-backend"
   description = "Contains CI firmware artefacts"
-  bucket_name = google_storage_bucket.armored_witness_firmware_ci.name
+  bucket_name = "armored-witness-firmware-ci" # google_storage_bucket.armored_witness_firmware_ci.name
   enable_cdn  = false
 }
 
@@ -225,127 +178,14 @@ resource "google_compute_global_network_endpoint" "distributor" {
   fqdn                          = var.distributor_host
 }
 
-# KMS key rings
-resource "google_kms_key_ring" "firmware_release_ci" {
-  location = var.signing_keyring_location
-  name     = "firmware-release-ci"
-}
-resource "google_kms_key_ring" "firmware_release_prod" {
-  location = var.signing_keyring_location
-  name     = "firmware-release-prod"
-}
-
-# TODO(jayhou): This configuration cannot be applied right now because of the
-# algorithm. Uncomment again when it is supported.
-### KMS keys
-resource "google_kms_crypto_key" "bootloader_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "bootloader-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "recovery_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "recovery-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "trusted_applet_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "trusted-applet-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "trusted_os_1_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "trusted-os-1-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "trusted_os_2_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "trusted-os-2-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "ft_log_ci" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-ci"
-  name                       = "ft-log-ci"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "bootloader_prod" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-prod"
-  name                       = "bootloader-prod"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "recovery_prod" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-prod"
-  name                       = "recovery-prod"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "trusted_applet_prod" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-prod"
-  name                       = "trusted-applet-prod"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "trusted_os_prod" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-prod"
-  name                       = "trusted-os-prod"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-resource "google_kms_crypto_key" "ft_log_prod" {
-  key_ring                   = "projects/armored-witness/locations/global/keyRings/firmware-release-prod"
-  name                       = "ft-log-prod"
-  purpose                    = "ASYMMETRIC_SIGN"
-  version_template {
-    algorithm        = "EC_SIGN_ED25519"
-    protection_level = "SOFTWARE"
-  }
-}
-
 resource "google_kms_key_ring" "terraform_state" {
-  name = "armored-witness-bucket-tfstate"
+  name     = "armored-witness-bucket-tfstate"
   location = var.tf_state_location
 }
 
 resource "google_kms_crypto_key" "terraform_state_bucket" {
-  name            = "terraform-state-bucket"
-  key_ring        = google_kms_key_ring.terraform_state.id
+  name     = "terraform-state-bucket"
+  key_ring = google_kms_key_ring.terraform_state.id
 }
 
 resource "google_storage_bucket" "terraform_state" {
@@ -361,4 +201,3 @@ resource "google_storage_bucket" "terraform_state" {
   }
   uniform_bucket_level_access = true
 }
-# TODO(jayhou): add GCF stuff.
--- deployment/api_transparency_dev/tls.tf
+++ deployment/api_transparency_dev/tls.tf
@@ -1,7 +1,7 @@
 resource "google_compute_managed_ssl_certificate" "lb_default" {
   provider = google-beta
   name     = "transparency-dev-ssl-cert"
-  project = var.project_id
+  project  = var.project_id
 
   managed {
     domains = [var.serve_domain]
