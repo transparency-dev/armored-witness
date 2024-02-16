b40cede458d05d9d1f335c84b21dc162f4da8f77
Jay <jayhou@google.com>
2024-02-16 12:35:21 +0000

    Add per-env state buckets.

--- deployment/build_and_release/modules/variables.tf
+++ deployment/build_and_release/modules/variables.tf
@@ -7,6 +7,11 @@ variable "env" {
   type        = string
 }
 
+variable "bucket_env" {
+  description = "Identifier for the env to use for existing buckets, e.g. '-ci' or ''. TODO: migrate/rename these resources."
+  type        = string
+}
+
 variable "signing_keyring_location" {
   description = "The GCP location to create the signing keyring"
 }
