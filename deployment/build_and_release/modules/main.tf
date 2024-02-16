91e71843dbb3932e22afce5c274a12240a03e722
Jay <jayhou@google.com>
2024-02-16 12:10:43 +0000

    Delete legacy state bucket from Terraform config.

--- deployment/build_and_release/modules/main.tf
+++ deployment/build_and_release/modules/main.tf
@@ -221,31 +221,4 @@ resource "google_kms_key_ring" "firmware_release" {
 #  }
 #}
 
-############################################################
-## Terraform state bucket
-############################################################
-
-resource "google_kms_key_ring" "terraform_state" {
-  name     = "armored-witness-bucket-tfstate"
-  location = var.tf_state_location
-}
-
-resource "google_kms_crypto_key" "terraform_state_bucket" {
-  name     = "terraform-state-bucket"
-  key_ring = google_kms_key_ring.terraform_state.id
-}
-
-resource "google_storage_bucket" "terraform_state" {
-  name          = "armored-witness-bucket-tfstate"
-  force_destroy = false
-  location      = var.tf_state_location
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
-  }
-  uniform_bucket_level_access = true
-}
 # TODO(jayhou): add GCF stuff.
