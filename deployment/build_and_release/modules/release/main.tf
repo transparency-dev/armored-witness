3ab60f6691a2f6e114af2fd65f61c708a367f81e
Al Cutter <al@google.com>
2024-04-23 16:24:38 +0100

    Verify build reproducibility

--- deployment/build_and_release/modules/release/main.tf
+++ deployment/build_and_release/modules/release/main.tf
@@ -390,6 +390,16 @@ resource "google_cloudbuild_trigger" "applet_build" {
       name = "bash"
       script = "cat output/trusted_applet_manifest"
     }
+    # Verify build reproducibility before we log anything...
+    step {
+      name = "golang"
+      entrypoint = "bash"
+      args = [
+        "-c",
+        <<-EOT
+        cat output/trusted_applet_manifest | go run ./cmd/verify_build single --tamago_dir=$HOME/tamago --v=2
+      ]
+    }
     ### Write the firmware release to the CI transparency log.
     # Copy the signed note to the sequence bucket, preparing to write to log.
     #
@@ -622,6 +632,16 @@ resource "google_cloudbuild_trigger" "os_build" {
     step {
       name = "bash"
       script = "cat output/trusted_os_manifest_both"
+    }
+    # Verify build reproducibility before we log anything...
+    step {
+      name = "golang"
+      entrypoint = "bash"
+      args = [
+        "-c",
+        <<-EOT
+        cat output/trusted_os_manifest_both | go run ./cmd/verify_build single --tamago_dir=$HOME/tamago --v=2
+      ]
     }
     ### Write the firmware release to the CI transparency log.
     # Copy the signed note to the sequence bucket, preparing to write to log.
@@ -894,6 +914,16 @@ resource "google_cloudbuild_trigger" "build_recovery" {
     step {
       name = "bash"
       script = "cat output/recovery_manifest"
+    }
+    # Verify build reproducibility before we log anything...
+    step {
+      name = "golang"
+      entrypoint = "bash"
+      args = [
+        "-c",
+        <<-EOT
+        cat output/recovery_manifest | go run ./cmd/verify_build single --tamago_dir=$HOME/tamago --v=2
+      ]
     }
     ### Write the firmware release to the CI transparency log.
     # Copy the signed note to the sequence bucket, preparing to write to log.
@@ -1177,6 +1207,16 @@ resource "google_cloudbuild_trigger" "build_boot" {
     step {
       name = "bash"
       script = "cat output/boot_manifest"
+    }
+    # Verify build reproducibility before we log anything...
+    step {
+      name = "golang"
+      entrypoint = "bash"
+      args = [
+        "-c",
+        <<-EOT
+        cat output/boot_manifest | go run ./cmd/verify_build single --tamago_dir=$HOME/tamago --v=2
+      ]
     }
     ### Write the firmware release to the CI transparency log.
     # Copy the signed note to the sequence bucket, preparing to write to log.
